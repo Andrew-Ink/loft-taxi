397409da8c498cf55cc8aa6789033c91bc314027
Andrey Meshcheryakov <inkognito213@gmail.com>
2022-03-07 10:01:50 +0300

    Задание первой недели

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Login } from './components/login/login';
+import { Map } from './components/map/map';
+import { Profile } from './components/profile/profile';
+import { Registration } from './components/registration/registration';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const PAGES = {
+  login: Login,
+  map: Map,
+  profile: Profile,
+  registration: Registration
+}
+
+class App extends React.Component {
+
+  state = {
+    currentPage: 'login'
+  }
+
+  navigateTo(page) {
+    this.setState({ currentPage: page })
+  }
+
+  render() {
+
+    const Page = PAGES[this.state.currentPage]
+
+    return (
+      <>{ !(this.state.currentPage === 'login' || this.state.currentPage === 'registration') && 
+        <header className="header" >
+          <nav className="menu">
+            <ul className="menu__list">
+              <li className="menu__item">
+                <button className="button" onClick={() => {
+                  this.navigateTo("map")
+                }}>
+                  Карта
+                </button>
+              </li>
+              <li className="menu__item">
+                <button className="button" onClick={() => {
+                  this.navigateTo("profile")
+                }}>
+                  Профиль
+                </button>
+              </li>
+              <li className="menu__item">
+                <button className="button" onClick={() => {
+                  this.navigateTo("login")
+                }}>
+                  Выйти
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </header> }
+        <main className="main-content">
+          <section className="section">
+            <Page navigateTo = {this.navigateTo.bind(this)} />
+          </section>
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
